3c81810db774991cf174ac80982a49fd0ead08f4
fryilmaztester <framazanyilmaz@gmail.com>
2020-11-08 19:14:05 +0300

    Third commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 
 		System.out.println("First commit from local Master Branch");
 		System.out.println("Second commit from local Master");
+		System.out.println("Third commit from local master");
 
 	}
 
